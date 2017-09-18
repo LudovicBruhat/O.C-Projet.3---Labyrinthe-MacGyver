3df798090fff9a1bfca54872c833153d830296dc
Interludo <ludovic.bruhat@gmail.com>
2017-09-18 12:41:45 +0200

    Finitions/Pepp8

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 """Class for the game"""
 
 
-import pygame  # peut être pas besoin
+import pygame  # needed ?
 
 from pygame.locals import *
 
@@ -34,34 +34,26 @@ class Char:
 
         if direction == 'left':
             if self.case_x > 0:
-                if self.level.structure[self.case_y][self.case_x-1] != 'm':
+                if self.level.structure[self.case_y][self.case_x - 1] != 'm':
                     self.case_x -= 1
                     self.x = self.case_x * Sprite_Size
                     print(self.x, self.y)
 
         if direction == 'up':
             if self.case_y > 0:
-                if self.level.structure[self.case_y-1][self.case_x] !='m': 
-                    if self.level.structure[self.case_y-1][self.case_x] !='c': 
+                if self.level.structure[self.case_y - 1][self.case_x] != 'm':
+                    if self.level.structure[self.case_y - 1][self.case_x] != 'c':
                         self.case_y -= 1
                         self.y = self.case_y * Sprite_Size
                         print(self.x, self.y)
 
         if direction == 'down':
-            if self.case_y < (Nbr_Sprite_Side ):
-                if self.level.structure[self.case_y+1][self.case_x] !='m':
+            if self.case_y < (Nbr_Sprite_Side):
+                if self.level.structure[self.case_y+1][self.case_x] != 'm':
                     self.case_y += 1
                     self.y = self.case_y * Sprite_Size
                     print(self.x, self.y)
 
-                """if event.key == K_DOWN: 
-                    position_Char = position_Char.move(0,60) 
-                if event.key == K_UP:
-                    position_Char = position_Char.move(0,-60)
-                if event.key == K_LEFT:
-                    position_Char = position_Char.move(-60,0)
-                if event.key == K_RIGHT:
-                    position_Char = position_Char.move(60,0)"""
 
 class Level:
     """class used for the maze"""
@@ -70,18 +62,16 @@ class Level:
         self.structure = 0
 
     def generate(self):
-        with open(self.file, "r") as file:
-            level_structure = []
+        with open(self.file, "r") as file: # Opening Level.txt as only readable
+            level_structure = [] # defining level structure as an empty list
 
             for line in file:
                 line_level = []
                 for sprite in line:
-                    if sprite !='\n':
-                        line_level.append(sprite)
-                level_structure.append(line_level)
-            self.structure = level_structure
-
-
+                    if sprite != '\n':
+                        line_level.append(sprite) # We had each sprite to the line level_level list, except for \n
+                level_structure.append(line_level) # We append the line_level list to the level_structure_list
+            self.structure = level_structure # we re-define the structure
 
     def display(self, Window):
         wall = pygame.image.load('images/wall.png').convert()
@@ -90,41 +80,32 @@ class Level:
         num_line = 0
         for line in self.structure:
             num_case = 0
-            for sprite in line:
-                x = num_case * Sprite_Size
+            for sprite in line:  # We check for each sprite in line
+                x = num_case * Sprite_Size  # each sprites position is calculated
                 y = num_line * Sprite_Size
-                if sprite == 'm':
-                    Window.blit(wall, (x,y))
-                elif sprite == 'a':
-                    Window.blit(badguy, (x,y))
-                """elif sprite == 'c':
-                    Window.blit(CarreNoir, (x,y))"""
-                num_case += 1
-            num_line += 1
+                if sprite == 'm':  # m = wall
+                    Window.blit(wall, (x, y))  # We blit the wall-img to the position on the case
+                elif sprite == 'a':  # a = BadGuy (exit)
+                    Window.blit(badguy, (x, y))  # We blit the badguy-img
+                num_case += 1  # once done with a sprite, we follow with the next entry
+            num_line += 1  # once done with a line, we follow with the next
 
 
-class loot: #the class for the items
+class loot:  # the class for the items
     def __init__(self, Loot_Image, level):
         self.case_y = 0
         self.case_x = 0
         self.x = 0
         self.y = 0
         self.level = level
-        self.loaded = True        
+        self.loaded = True
         self.Loot_Image = Loot_Image
 
     def display(self, Loot_Image, Window):
-        while self.loaded :
-            self.case_x = random.randint(0,14)
-            self.case_y = random.randint(0,14)
-            if self.level.structure[self.case_y][self.case_x] == '0':#l'inversement de self.case_x, self.case_y permet de faire ce que je veux !!! (l'inverse fais apparaitre les objets dans les walls. Youpi !)
-                self.y = self.case_y * Sprite_Size
+        while self.loaded:
+            self.case_x = random.randint(0, 14)  # We randomize the case_x position
+            self.case_y = random.randint(0, 14)  # same for case_y position
+            if self.level.structure[self.case_y][self.case_x] == '0': # if the randomized position is located on a free space
+                self.y = self.case_y * Sprite_Size  # We define/accept the position for the object
                 self.x = self.case_x * Sprite_Size
-                self.loaded = False
-                
-
-
-                """if self.level.structure[self.case_x][self.case_y] != 'm':
-                if self.level.structure[self.case_x][self.case_y] != 'a':
-                    if self.level.structure[self.case_x][self.case_y] != 'd':"""
-
+                self.loaded = False  # Once we have defined a position for one object, the script is over
--- constantes.py
+++ constantes.py
@@ -1,15 +1,15 @@
-# Personnalisation de la fenêtre
+# We set the window properties
 
 Nbr_Sprite_Side = 15
 Sprite_Size = 30
-Window_Size = Nbr_Sprite_Side * Sprite_Size  # à mettre en majuscule
+Window_Size = Nbr_Sprite_Side * Sprite_Size  # Window size is equal to the number of sprites (15) * their size in pixel (30px)
 
-Window_Title = "Aider MacGyver à s'échapper"
+Window_Title = "Save MacGyver !"
 
 icone = "images/macgyver.png"
 
 
-# Listes des images du jeu
+# List of the game images
 
 background = "images/background.jpg"
 
